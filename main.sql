e3391c335f042f4cf25acffe259ed6b72976f75f
Gage Eveland <42362153+EvelAndG@users.noreply.github.com>
2021-12-01 16:40:06 +0000

    Adding employees

--- main.sql
+++ main.sql
@@ -1,16 +1,17 @@
 .headers "on"
 .mode "column"
 
---Question 1 Table
+--Question 1 Table Creation
 CREATE TABLE employee (
 	employee_id INTEGER PRIMARY KEY AUTOINCREMENT,
 	first_name varchar,
 	last_name varchar,
 	job_title varchar,
 	stocks INTEGER
+	
 );
 
---Question 2 Table
+--Question 2 Table Creation
 CREATE TABLE certification (
 	cert_id INTEGER PRIMARY KEY AUTOINCREMENT,
 	cert_name varchar,
@@ -18,9 +19,20 @@ CREATE TABLE certification (
 	price INTEGER
 );
 
+--Question 3 Table Creation
+CREATE TABLE credential (
+	employee_id INTEGER
+
+);
+
 --Inserting into table 1
 INSERT INTO employee (first_name, last_name, job_title, stocks)
-VALUES ('George', 'Costanza', 'Architect', 100);
+VALUES ('George', 'Costanza', 'Architect', 100),
+('Jerry', 'Seinfeld', 'Comedian', 1000),
+('Cosmo', 'Kramer', 'Neighbor', 50),
+('Elaine', 'Benes', 'Writer', 400),
+('Norman', 'Newman', 'Postman', '300')
+;
 
 --Inserting into table 2
 INSERT INTO certification (cert_name, category, price)
